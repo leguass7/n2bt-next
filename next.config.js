aa83b25d790b8d928d28e89c5d1a0f060832da0f
Leandro Sbrissa <leandro.sbrissa@hotmail.com>
2022-08-14 10:38:31 -0300

    docs: teste

--- next.config.js
+++ next.config.js
@@ -1,7 +1,29 @@
+/* @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+// }
+
+// module.exports = nextConfig
+
+// const withPlugins = require("next-compose-plugins");
+// const withFonts = require("next-fonts");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  optimizeFonts: false,
+  compiler: {
+    styledComponents: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+// module.exports = withPlugins(
+//   [
+//     [withFonts],
+//     // ...
+//   ],
+//   nextConfig
+// );
--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,24 +1,3 @@
-// import type { NextApiRequest, NextApiResponse } from 'next'
-// import { prepareDataSource } from '../../server-side/database'
-// import { User } from '../../server-side/userCases/user/user.entity';
-// import { instanceToPlain } from 'class-transformer';
-
-// type Data = Record<string, any> & {
-//   success: boolean
-// }
-
-// export default async function handler(
-//   req: NextApiRequest,
-//   res: NextApiResponse<Data>
-// ) {
-//   const dataSource = await prepareDataSource();
-//   const repo = dataSource.getRepository(User);
-//   const users = await repo.find({ select: { id: true, name: true }})
-//   // dataSource.destroy()
-
-//   res.status(200).json({ success: !!dataSource, users: users.map(u => instanceToPlain(u)) })
-// }
-
 import { createHandler, Get, HttpCode, Req } from '@storyofams/next-api-decorators';
 import { instanceToPlain } from 'class-transformer';
 import type { NextApiRequest } from 'next';
