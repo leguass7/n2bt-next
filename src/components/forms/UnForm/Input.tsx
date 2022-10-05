0a01724977c63774d460f34d116b3aaf4c79b51d
Emanuel Severo <fs.severoemanuel@gmail.com>
2022-10-05 18:13:50 +0000

    fix: unform input validation of number type

--- src/components/forms/UnForm/Input.tsx
+++ src/components/forms/UnForm/Input.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useRef } from 'react'
+import React, { InputHTMLAttributes, useCallback, useEffect, useRef } from 'react'
 
 import { InputProps } from '@mui/material/Input'
 import TextField from '@mui/material/TextField'
@@ -12,29 +12,35 @@ interface Props extends InputProps {
 }
 
 export const Input: React.FC<Props> = ({ name, type = 'text', id, label, number, onChange, placeholder, multiline }) => {
-  const ref = useRef<HTMLInputElement>(null)
+  const ref = useRef<InputHTMLAttributes<HTMLInputElement>>(null)
   const { defaultValue, fieldName, registerField, error } = useField(name)
 
   useEffect(() => {
     registerField({
       name: fieldName,
       ref: ref.current,
-      path: 'value',
+      // path: 'value',
       clearValue() {
         ref.current.value = ''
+      },
+      getValue(input) {
+        if (number) return Number(`${input.value ?? 0}`)
+        return input?.value
       }
     })
-  }, [registerField, fieldName])
+  }, [registerField, fieldName, number])
 
   const handleChange = useCallback(
     e => {
       let value: string = e.target.value
       if (number) {
+        // Only lets value have 1 dot
         if (value.includes('.')) {
-          // Only lets value have 1 dot
           value = `${value.split('.')[0]}.${value.split('.')[1].replace(/\./, '')}`
         }
-        ref.current.value = value.replace(/[^0-9.]/g, '')
+        value = value.replace(/[^0-9.]/g, '')
+
+        ref.current.value = value
       }
       if (onChange) onChange(e)
     },
