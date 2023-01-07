d375e3da2cb23bc0fde5298b3c2408dfea64c50d
severoemanuel <46508229+severoemanuel@users.noreply.github.com>
2023-01-07 16:00:55 -0300

    fix(api): play field routes

--- src/services/api/play-field.ts
+++ src/services/api/play-field.ts
@@ -1,18 +1,18 @@
+import { PlayFieldFormData } from '~/components/admin/PlayFields/PlayFieldForm'
 import { NewResponseApi } from '~/server-side/api.interface'
-import { CreatePlayFieldDTO } from '~/server-side/useCases/play-field/dto/create-play-field.dto'
 import { FilterPlayFieldDTO } from '~/server-side/useCases/play-field/dto/filter-play-field.dto'
 import { UpdatePlayFieldDTO } from '~/server-side/useCases/play-field/dto/update-play-field.dto'
 import { PlayField } from '~/server-side/useCases/play-field/play-field.entity'
 
 import { apiService } from './api.service'
 
-export async function createPlayField(dto: CreatePlayFieldDTO): Promise<NewResponseApi<PlayField>> {
+export async function createPlayField(dto: PlayFieldFormData): Promise<NewResponseApi<PlayField>> {
   const response = await apiService.post(`/play-field`, dto)
   return response
 }
 
-export async function updatePlayField(dto: UpdatePlayFieldDTO): Promise<NewResponseApi<any>> {
-  const response = await apiService.patch(`/play-field`, dto)
+export async function updatePlayField(fieldId: number, dto: UpdatePlayFieldDTO): Promise<NewResponseApi<any>> {
+  const response = await apiService.patch(`/play-field/${fieldId}`, dto)
   return response
 }
 
@@ -25,6 +25,6 @@ export async function listPlayFieldInArena(arenaId: number, filter: FilterPlayFi
 }
 
 export async function getPlayField(fieldId: number): Promise<NewResponseApi<PlayField>> {
-  const response = await apiService.get(`/play-field$/${fieldId}`)
+  const response = await apiService.get(`/play-field/${fieldId}`)
   return response
 }
