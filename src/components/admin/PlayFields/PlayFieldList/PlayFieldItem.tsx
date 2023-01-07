e43d94ef04461b2ce41e8c85f12277f60a0665e1
severoemanuel <46508229+severoemanuel@users.noreply.github.com>
2023-01-07 16:01:22 -0300

    feat: crud of play fields in admin

--- src/components/admin/PlayFields/PlayFieldList/PlayFieldItem.tsx
+++ src/components/admin/PlayFields/PlayFieldList/PlayFieldItem.tsx
@@ -1,14 +1,20 @@
 import { useMemo } from 'react'
 
-import { Card, CardHeader } from '@mui/material'
+import Delete from '@mui/icons-material/Delete'
+import Edit from '@mui/icons-material/Edit'
+import { Card, CardActions, CardHeader, Grid, IconButton, Typography } from '@mui/material'
 import { isAfter, isBefore } from 'date-fns'
 import Image from 'next/image'
 
+import { formatPrice } from '~/helpers'
 import type { PlayField } from '~/server-side/useCases/play-field/play-field.entity'
 
-interface Props extends PlayField {}
+interface Props extends PlayField {
+  onEdit?: (id: number) => void
+  onDelete?: (id: number) => void
+}
 
-export const PlayFieldItem: React.FC<Props> = ({ label, startDate, endDate }) => {
+export const PlayFieldItem: React.FC<Props> = ({ id, label, startDate, endDate, price, onEdit, onDelete }) => {
   const isInUse = useMemo(() => {
     const now = new Date()
 
@@ -21,6 +27,26 @@ export const PlayFieldItem: React.FC<Props> = ({ label, startDate, endDate }) =>
     <Card>
       <CardHeader title={label} subheader={subheader} />
       <Image src="/layout.png" layout="responsive" width="80%" height={100} alt={label} />
+      <CardActions>
+        <Grid container justifyContent="space-between" alignItems="center">
+          <Grid item>
+            <Typography variant="body1">{formatPrice(price)}</Typography>
+          </Grid>
+          <Grid item>
+            {onEdit ? (
+              <IconButton onClick={() => onEdit(id)}>
+                <Edit />
+              </IconButton>
+            ) : null}
+
+            {onDelete ? (
+              <IconButton onClick={() => onDelete(id)}>
+                <Delete />
+              </IconButton>
+            ) : null}
+          </Grid>
+        </Grid>
+      </CardActions>
     </Card>
   )
 }
--- src/components/admin/PlayFields/PlayFieldList/index.tsx
+++ src/components/admin/PlayFields/PlayFieldList/index.tsx
@@ -1,13 +1,17 @@
 import { useCallback, useEffect, useState } from 'react'
 
-import { Grid } from '@mui/material'
+import { Add } from '@mui/icons-material'
+import { Grid, IconButton, Typography } from '@mui/material'
 
 import { CircleLoading } from '~/components/CircleLoading'
+import { SimpleModal } from '~/components/Common/SimpleModal'
 import { useIsMounted } from '~/hooks/useIsMounted'
 import type { PlayField } from '~/server-side/useCases/play-field/play-field.entity'
 import { listPlayFieldInArena } from '~/services/api/play-field'
 import { Content } from '~/styles'
 
+import { PlayFieldDelete } from '../PlayFieldDelete'
+import { PlayFieldForm } from '../PlayFieldForm'
 import { PlayFieldItem } from './PlayFieldItem'
 
 interface Props {
@@ -21,6 +25,10 @@ export const PlayFieldList: React.FC<Props> = ({ arenaId }) => {
   const [loading, setLoading] = useState(false)
   const isMounted = useIsMounted()
 
+  const [fieldId, setFieldId] = useState(null)
+  const [openFormModal, setOpenFormModal] = useState(false)
+  const [openDeleteModal, setOpenDeleteModal] = useState(false)
+
   const fetchData = useCallback(async () => {
     if (!arenaId) return null
     setLoading(true)
@@ -29,7 +37,7 @@ export const PlayFieldList: React.FC<Props> = ({ arenaId }) => {
 
     if (isMounted()) {
       setLoading(false)
-      if (data?.length) setFields(data.concat(data).concat(data).concat(data).concat(data).concat(data).concat(data))
+      if (data?.length) setFields(data)
     }
   }, [isMounted, arenaId])
 
@@ -37,21 +45,69 @@ export const PlayFieldList: React.FC<Props> = ({ arenaId }) => {
     fetchData()
   }, [fetchData])
 
+  const toggleFormModal = useCallback(() => setOpenFormModal(old => !old), [])
+  const toggleDeleteModal = useCallback(() => setOpenDeleteModal(old => !old), [])
+
+  const handleEdit = useCallback(
+    (id?: number) => {
+      setFieldId(id)
+      toggleFormModal()
+    },
+    [toggleFormModal]
+  )
+
+  const handleDelete = useCallback(
+    (id?: number) => {
+      setFieldId(id)
+      toggleDeleteModal()
+    },
+    [toggleDeleteModal]
+  )
+
+  const handleToggleForm = useCallback(() => {
+    setFieldId(null)
+    fetchData()
+
+    toggleFormModal()
+  }, [toggleFormModal, fetchData])
+
+  const handleToggleDelete = useCallback(() => {
+    setFieldId(null)
+    fetchData()
+
+    toggleDeleteModal()
+  }, [toggleDeleteModal, fetchData])
+
   return (
     <Content>
+      <Grid container alignItems="center" justifyContent="space-between">
+        <span />
+        <Typography variant="h4" align="center" py={2}>
+          Campos
+        </Typography>
+        <IconButton onClick={toggleFormModal}>
+          <Add />
+        </IconButton>
+      </Grid>
       <Grid container py={4} spacing={3}>
         {fields?.length
           ? fields.map(({ id, ...rest }) => {
               return (
                 <>
                   <Grid item py={2} xs={12} sm={6} md={4} key={`field-${id}`}>
-                    <PlayFieldItem id={id} {...rest} />
+                    <PlayFieldItem id={id} onEdit={handleEdit} onDelete={handleDelete} {...rest} />
                   </Grid>
                 </>
               )
             })
           : null}
       </Grid>
+      <SimpleModal open={openFormModal} onToggle={handleToggleForm} title="Criar/Editar campo">
+        <PlayFieldForm arenaId={arenaId} onSuccess={handleToggleForm} fieldId={fieldId} />
+      </SimpleModal>
+      <SimpleModal open={openDeleteModal} onToggle={handleToggleDelete} title="Deseja mesmo excluir esse campo?">
+        <PlayFieldDelete onSuccess={handleToggleDelete} fieldId={fieldId} />
+      </SimpleModal>
       {loading ? <CircleLoading /> : null}
     </Content>
   )
