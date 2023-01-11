da051ce9990c15a7bbc7468e223b688ded4c91af
severoemanuel <46508229+severoemanuel@users.noreply.github.com>
2023-01-11 20:50:21 -0300

    feat(db): added vollei ball with partner modality to tournament

--- src/server-side/useCases/tournament/tournament.dto.ts
+++ src/server-side/useCases/tournament/tournament.dto.ts
@@ -18,5 +18,6 @@ export interface IResponseTournaments extends IResponseApi {
 export enum TournamentModality {
   BEACH_TENNIS = 'BEACH_TENNIS',
   BEACH_VOLEI = 'BEACH_VOLEI',
+  BEACH_VOLEI_WITH_PARTNER = 'BEACH_VOLEI_WITH_PARTNER',
   FUTVOLEI = 'FUTVOLEI'
 }
