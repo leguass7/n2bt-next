453843a801f375b15135ae33db37a45333759ba2
Leandro Sbrissa <leandro.sbrissa@hotmail.com>
2023-04-21 13:01:51 -0300

    feat(image): refatorado Card de torneio
    card do torneio necessita de apenas uma imagem, e não uma lista

--- src/services/api/image.ts
+++ src/services/api/image.ts
@@ -1,8 +1,13 @@
-import type { ImageFeature, IResponseGetImages } from '~/server-side/useCases/image/image.dto'
+import type { ImageFeature, IResponseGetImage, IResponseGetImages } from '~/server-side/useCases/image/image.dto'
 
 import { apiService } from './api.service'
 
 export async function getImageFromFeature(feature: ImageFeature, featureId: number): Promise<IResponseGetImages> {
   const response = await apiService.get(`/image/${feature}/${featureId}`)
+  return response
+}
+
+export async function getImageByTournamentId(tournamentId: number): Promise<IResponseGetImage> {
+  const response = await apiService.get(`/image/tournament/${tournamentId}`)
   return response
 }
