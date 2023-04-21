2264ec9fae1a04b32f2bea2a958af7c520aaa302
Leandro Sbrissa <leandro.sbrissa@hotmail.com>
2023-04-21 15:54:48 -0300

    feat(auth): decorator para rota admin

--- src/server-side/useCases/auth/middleware.ts
+++ src/server-side/useCases/auth/middleware.ts
@@ -4,6 +4,7 @@ import { getToken } from 'next-auth/jwt'
 import { getSession } from 'next-auth/react'
 import { parse } from 'next-useragent'
 
+import { makeArray } from '~/helpers/array'
 import { secret } from '~/server-side/config'
 
 import type { AuthorizedApiRequest } from './auth.dto'
@@ -51,3 +52,23 @@ export const IfAuth = createMiddlewareDecorator(async (req: AuthorizedApiRequest
     next()
   }
 })
+
+export const AdminAuth = (levels: number[] | number) =>
+  createMiddlewareDecorator(async (req: AuthorizedApiRequest, res: NextApiResponse, next: NextFunction) => {
+    const unauthorize = (msg = 'unauthorized') => next(new UnauthorizedException(msg))
+    try {
+      let session = await getToken({ req, secret })
+      if (!session) {
+        session = await getSession({ req })
+      }
+
+      req.auth = session ? authorizedDto(session) : null
+      req.ua = req?.headers['user-agent'] ? parse(req.headers['user-agent']) : null
+
+      if (!req?.auth?.level || !makeArray(levels).includes(req.auth.level)) return unauthorize()
+
+      next()
+    } catch (error) {
+      unauthorize()
+    }
+  })
