62cba75999eeb6af801ae935804c2a41bd0b2051
Leandro Sbrissa <leandro.sbrissa@hotmail.com>
2023-04-22 12:35:43 -0300

    feat(promo-code): copiar link no admin

--- src/components/admin/TablePromoCode/Cells.tsx
+++ src/components/admin/TablePromoCode/Cells.tsx
@@ -1,6 +1,8 @@
-import React, { memo } from 'react'
+import React, { memo, useState } from 'react'
 
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever'
 import EditIcon from '@mui/icons-material/Edit'
+import LinkIcon from '@mui/icons-material/Link'
 import { IconButton, Toolbar, Tooltip } from '@mui/material'
 
 import type { ICustomCellProps } from '~/components/CustomTable'
@@ -19,29 +21,59 @@ const DateTime: React.FC<Props> = ({ record }) => {
 
   return (
     <CellContainer>
-      <Text textSize={14}>{date}</Text>
-      <br />
-      <Text textSize={12}>{time}</Text>
+      <Text textSize={14} align="right">
+        {date}
+      </Text>
+      <Text textSize={12} align="right">
+        {time}
+      </Text>
     </CellContainer>
   )
 }
 export const DateTimeCell = memo(DateTime)
 
 export const OptionTools: React.FC<Props> = ({ record }) => {
+  const [tipOpen, setTipOpen] = useState(false)
   const { custom, setCustom } = useTableActions<IPromoCodeActions>()
 
-  const handleEdit = () => setCustom({ editId: record?.id })
+  const strLink = `/subscription?tournamentId=${record.tournamentId}&promo=${record.code}`
+
+  const handleClickDelete = () => setCustom({ deleteId: record?.id })
+  const handleClickEdit = () => setCustom({ editId: record?.id })
+  const handleClickCopy = () => {
+    if (strLink) navigator?.clipboard?.writeText(strLink)
+    setTipOpen(true)
+  }
   const disabled = !!custom?.editId
 
   return (
     <CellContainer>
-      <Toolbar>
-        <IconButton onClick={handleEdit} disabled={disabled}>
+      <Toolbar sx={{ gap: 1 }}>
+        <IconButton onClick={handleClickEdit} disabled={disabled}>
           <Tooltip title="Alterar" arrow>
             <EditIcon />
           </Tooltip>
         </IconButton>
+        <IconButton onClick={handleClickCopy} disabled={disabled}>
+          <Tooltip open={!!tipOpen} title="Link copiado" arrow onMouseLeave={() => setTipOpen(false)}>
+            <LinkIcon />
+          </Tooltip>
+        </IconButton>
+        <IconButton onClick={handleClickDelete} disabled={disabled}>
+          <Tooltip title="Remover código" arrow>
+            <DeleteForeverIcon />
+          </Tooltip>
+        </IconButton>
       </Toolbar>
+    </CellContainer>
+  )
+}
+
+export const LimitCell: React.FC<Props> = ({ record }) => {
+  const used = record?.payments?.length || 0
+  return (
+    <CellContainer>
+      <Text textSize={14}>{`${used}/${record?.usageLimit}`}</Text>
     </CellContainer>
   )
 }
--- src/components/admin/TablePromoCode/columns.tsx
+++ src/components/admin/TablePromoCode/columns.tsx
@@ -1,12 +1,12 @@
 import type { IColumnTable } from '~/components/CustomTable'
 import type { IPromoCode } from '~/server-side/useCases/promo-code/promo-code.dto'
 
-import { DateTimeCell, OptionTools } from './Cells'
+import { DateTimeCell, LimitCell, OptionTools } from './Cells'
 
 export const columns: IColumnTable<IPromoCode>[] = [
   { name: 'label', label: 'Rótulo' },
   { name: 'code', label: 'Código' },
-  { name: 'usageLimit', label: 'Limite de uso', align: 'center' },
-  { name: 'createdAt', label: 'Data', Cell: DateTimeCell, width: 110 },
+  { name: 'usageLimit', label: 'Limite de uso', align: 'center', Cell: LimitCell },
+  { name: 'createdAt', label: 'Criado em', Cell: DateTimeCell, width: 130, align: 'right' },
   { Cell: OptionTools, width: 110 }
 ]
