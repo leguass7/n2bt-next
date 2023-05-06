5f7c58733de9dc0aa9771d220eaf4cdd3cd9deab
severoemanuel <46508229+severoemanuel@users.noreply.github.com>
2023-05-06 17:05:11 -0300

    feat: added payments info card in tournament reports page

--- src/components/admin/ShirtsReport/index.tsx
+++ src/components/admin/ShirtsReport/index.tsx
@@ -1,4 +1,5 @@
-import React, { useCallback, useState } from 'react'
+import React, { useCallback, useEffect, useMemo, useState } from 'react'
+import { Chart, GoogleChartOptions } from 'react-google-charts'
 import { toast } from 'react-toastify'
 
 import { ArrowBack, ContentCopy } from '@mui/icons-material'
@@ -6,21 +7,64 @@ import { Card, CardActions, CardContent, CardHeader, Divider, Grid, IconButton, 
 import Link from 'next/link'
 
 import { TableReport } from '~/components/admin/TableReport'
+import { themes } from '~/components/AppThemeProvider/themes'
 import { CircleLoading } from '~/components/CircleLoading'
+import { formatPrice } from '~/helpers'
 import { useIsMounted } from '~/hooks/useIsMounted'
 import { useOnceCall } from '~/hooks/useOnceCall'
+import { Payment } from '~/server-side/useCases/payment/payment.entity'
 import type { ISubscription, ISubscriptionStatistics } from '~/server-side/useCases/subscriptions/subscriptions.dto'
+import { searchPayments } from '~/services/api/payment'
 import { getSubscriptionReport } from '~/services/api/subscriptions'
+
+const options: Partial<GoogleChartOptions> = {
+  backgroundColor: themes.common.colors.background,
+  title: 'Pagamentos',
+  legend: 'Pagamentos'
+}
+
+const getPaymentValues = (payment: Payment) => payment?.value || 0
+const sum = (prev: number, next: number): number => prev + next
 
 type Props = {
   tournamentId: number
 }
+
 export const ShirtsReport: React.FC<Props> = ({ tournamentId }) => {
   const [data, setData] = useState<ISubscription[]>([])
+  const [payments, setPayments] = useState<Payment[]>([])
   const [statistics, setStatistics] = useState<Partial<ISubscriptionStatistics>>({})
 
   const [loading, setLoading] = useState(false)
   const isMounted = useIsMounted()
+
+  const fetchPayments = useCallback(async () => {
+    if (!tournamentId) return
+
+    setLoading(true)
+    const response = await searchPayments({ tournamentId })
+
+    if (isMounted()) {
+      setLoading(false)
+      if (response?.payments?.length) setPayments(response?.payments)
+    }
+  }, [isMounted, tournamentId])
+
+  useEffect(() => {
+    fetchPayments()
+  }, [fetchPayments])
+
+  const paymentStatistics = useMemo(() => {
+    if (!payments?.length) return null
+
+    const paid = payments.filter(({ paid, actived }) => actived && paid).map(getPaymentValues)
+    const notPaid = payments.filter(({ paid, actived }) => actived && !paid).map(getPaymentValues)
+
+    const totalPaid = paid.reduce(sum, 0)
+    const totalNotPaid = notPaid.reduce(sum, 0)
+
+    return { totalPaid, totalNotPaid }
+  }, [payments])
 
   const fetchData = useCallback(
     async (filter?: Record<string, any>) => {
@@ -67,6 +111,17 @@ export const ShirtsReport: React.FC<Props> = ({ tournamentId }) => {
     }
   }
 
+  const handleCopyPaymentInfo = () => {
+    if (document) {
+      const containerInfo = document.getElementById('payments-info')
+      const info = containerInfo?.innerText?.replace(/\n\n/g, '\n')
+      if (info) {
+        navigator.clipboard.writeText(info)
+        toast('Conteúdo copiado com sucesso!', { type: 'info', position: 'bottom-right' })
+      }
+    }
+  }
+
   return (
     <>
       <Grid container py={2} justifyContent="space-between">
@@ -83,7 +138,7 @@ export const ShirtsReport: React.FC<Props> = ({ tournamentId }) => {
         <span />
       </Grid>
 
-      <Grid container>
+      <Grid container spacing={2}>
         <Grid item xs={12} sm={6} md={4} lg={3}>
           <Card>
             <CardHeader
@@ -102,6 +157,48 @@ export const ShirtsReport: React.FC<Props> = ({ tournamentId }) => {
                 Total: {statistics?.total}
               </Typography>
             </CardActions>
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={6} md={4} lg={3}>
+          <Card>
+            <CardHeader
+              title="Pagamentos"
+              action={
+                <IconButton onClick={handleCopyPaymentInfo}>
+                  <ContentCopy />
+                </IconButton>
+              }
+            />
+            <Divider />
+
+            <CardContent>
+              <Grid container spacing={2}>
+                <Grid item xs={12}>
+                  {paymentStatistics?.totalPaid || paymentStatistics?.totalNotPaid ? (
+                    <Chart
+                      chartType="PieChart"
+                      width="100%"
+                      legendToggle
+                      options={options}
+                      data={[
+                        ['Pago', 'Não pago'],
+                        ['Pago (R$)', paymentStatistics?.totalPaid || 0],
+                        ['Não pago (R$)', paymentStatistics?.totalNotPaid || 0]
+                      ]}
+                    />
+                  ) : null}
+                </Grid>
+                <Grid item xs={12} id="payments-info">
+                  <Grid container height="100%" alignItems="flex-end">
+                    <Typography variant="caption" fontWeight={700}>
+                      Total recebido: {formatPrice(paymentStatistics?.totalPaid || 0)}
+                      <br />
+                      Total a receber: {formatPrice(paymentStatistics?.totalNotPaid || 0)}
+                    </Typography>
+                  </Grid>
+                </Grid>
+              </Grid>
+            </CardContent>
           </Card>
         </Grid>
       </Grid>
