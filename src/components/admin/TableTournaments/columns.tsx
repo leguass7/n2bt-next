a0acae822b0432c22b3d2039a10d0186d7438f42
Leandro Sbrissa <leandro.sbrissa@hotmail.com>
2024-06-18 23:01:39 -0300

    refactor(admin): ajuste da tabela

--- src/components/admin/TableTournaments/columns.tsx
+++ src/components/admin/TableTournaments/columns.tsx
@@ -7,8 +7,8 @@ import { DateCell, DateTimeCell, NameCell, SwitchCell } from './Cells'
 export const columns: IColumnTable<ITournament>[] = [
   { name: 'title', label: 'Nome', Cell: NameCell },
 
-  { name: 'expires', label: 'Inicio', Cell: ({ record }) => <DateCell date={record?.expires} /> },
-  { name: 'subscriptionEnd', label: 'Inscrições até', Cell: ({ record }) => <DateCell date={record?.subscriptionEnd} /> },
+  { name: 'subscriptionStart', label: 'Inicio inscrições', Cell: ({ record }) => <DateCell date={record?.subscriptionStart} /> },
+  { name: 'subscriptionEnd', label: 'Fim inscrições', Cell: ({ record }) => <DateCell date={record?.subscriptionEnd} /> },
   { name: 'createdAt', label: 'criado em', Cell: DateTimeCell },
   { name: 'published', label: 'Ativo', align: 'center', width: 90, Cell: SwitchCell },
   { Cell: ActionMenuCell, width: 100, align: 'right' }
